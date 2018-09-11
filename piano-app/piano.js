b9e5ae1b9be5c146fb590c1e25c0478fbc4eb032
Shahid Khan <shahid.manit@gmail.com>
2018-09-11 08:11:22 -0500

    Piano app implementation
    Piano app implementation

---
+++ piano-app/piano.js
@@ -0,0 +1,17 @@
+function saregama(e){
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if(!audio) return;
+  audio.currentTime=0; // rewind to start
+  audio.play();
+  key.classList.add('play');
+}
+function removeTransition(e) {
+  if(e.propertyName !== 'transform') return;
+  this.classList.remove('play')
+}
+const keys = document.querySelectorAll('.key');
+keys.forEach(function(key){
+  key.addEventListener('transitionend', removeTransition);
+})
+window.addEventListener('keydown', saregama);
